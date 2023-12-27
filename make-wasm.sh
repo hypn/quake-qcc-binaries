09d2d2c9b28b8826aea54ad2bdf912b4b5ae667a
Ross Simpson <ross.simpson@gmail.com>
2023-12-27 05:07:22 +0200

    Updated wasm script

--- make-wasm.sh
+++ make-wasm.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-CMD='cd src; emcc -O3 -s WASM=1 -o ../qcc-wasm.html -I ../Quake/WinQuake *.c'
-
-# compile Linux binaries
-docker run -v `pwd`:/work dockcross/web-wasm bash -c "$CMD"
+docker run -v `pwd`:/work dockcross/web-wasm bash -c 'cd src; emcc -O3 -s WASM=1 -o ../qcc-wasm.html -I ../Quake/WinQuake *.c'
